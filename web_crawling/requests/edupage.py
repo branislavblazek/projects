865d712baa5824b211cddd2f5a05ad288cd62200
branislavblazek <branislav.blazek.bb@gmail.com>
2020-03-17 22:53:49 +0100

    znamky funguju

--- web_crawling/requests/edupage.py
+++ web_crawling/requests/edupage.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
-import time
+import re
+import json
 
 class Edupage:
     def __init__(self):
@@ -28,6 +29,8 @@ class Edupage:
 
         soup = BeautifulSoup(content, "html.parser")
         login_data['csrfauth'] = soup.find('input', attrs={'name': "csrfauth"})['value']
+        #niekedy nie je potrebne csrf auth
+        #login_data['csrfauth'] = ''
 
         self.session = requests.Session()
         self.session.post(login_url, data=login_data, headers=headers)
@@ -61,9 +64,48 @@ class Edupage:
                     bunka = riadok.find_all('td')
                     for info in bunka:
                         obsah.append(info.text)
-                    print(obsah)
+                    print(''.join(obsah))
 
+    def my_marks(self):
+        marks_url = 'https://gvoza.edupage.org/znamky/?what=studentviewer&nadobdobie=P2&doRq=1&what=studentviewer&updateLastView=0'
+        content = self.session.get(marks_url).content
+        soup = BeautifulSoup(content, 'html.parser')
+        scripts = soup.find_all('script', {'type': 'text/javascript'})
+        scripts = [script for script in scripts if len(script.attrs) == 1]
+        text_scriptu = scripts[2].text
+        whole_obj = re.search(r"\"vsetkyZnamky\":(\[[^\[\]]+\])", text_scriptu).group(1)
+
+        #toto dole zoberie nazvy predmetov
+        subjects = re.search(r"\"predmety\":(.+?(?=}})}})", text_scriptu).group(1)
+        predmety = json.loads(subjects)
+
+        #toto berie ucitelov
+        teachers = re.search(r"\"ucitelia\":(.+?(?=}})}})", text_scriptu).group(1)
+        ucitelia = json.loads(teachers)
+        
+        #toto berie znamky
+        marks = re.findall(r"\{[^\{\}]+\}", whole_obj)
+
+        znamky = [json.loads(mark) for mark in marks]
+        print('{:35} | {:^7} | {:<30} | {:^22} | {:}'.format('Prednet', 'Znamka', 'Meno ucitela', 'Datum vlozenia', 'Datum podpisania'))
+        print('{0:_^35} | {0:_^7} | {0:_<30} | {0:_^22} | {0:_^22}'.format(''))
+        for znamka in znamky:
+            data = {}
+            predmet = znamka['predmetid']
+            data['predmet'] = predmety[predmet]['p_meno']
+            data['hodnota'] = znamka['data']
+            ucitel = znamka['ucitelid']
+            data['ucitel'] = ucitelia[ucitel]['firstname'] + ' ' + ucitelia[ucitel]['lastname']
+            data['datum'] = znamka['datum']
+            data['podpisane'] = znamka['podpisane']
+            print('{predmet:35} | {hodnota:^7} | {ucitel:30} | {datum:^22} | {podpisane:}'.format(**data))
+
+
+        pass
 
 handler = Edupage()
 
 #handler.my_account()
+
+handler.my_marks()
+
