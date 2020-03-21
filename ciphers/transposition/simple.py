7de88a32831115229e14e9b122f8853713a0fede
branislavblazek <branislav.blazek.bb@gmail.com>
2020-03-21 18:41:57 +0100

    simple trans

--- ciphers/transposition/simple.py
+++ ciphers/transposition/simple.py
@@ -64,8 +64,64 @@ class Simple:
         encoded_text = ' '.join(encoded_text)
 
         return encoded_text
+    
+    def decode(self, input_text, key):
+        input_text = input_text.lower().lstrip().rstrip()
+        text = [letter for letter in input_text if letter in self.alphabet]
+        key = key.lower().lstrip().rstrip()
+        #len povolene znaky
+        key = [letter for letter in key if letter in self.alphabet]
+        #najdi opakovanie
+        perm_key = []
+        for index, letter in enumerate(key):
+            if index != 0:
+                if key[index-1] != letter:
+                    perm_key.append(letter)
+            else:
+                perm_key.append(letter)
 
+        perm_key = self.find_order(perm_key)
+        #vytvor tabulku
+        pocet_row = {}
+        for index in range(len(perm_key)):
+            dlzka = len(text) // len(perm_key)
+            if index < len(text) % len(perm_key):
+                dlzka += 1
+            pocet_row[perm_key[index]] = dlzka
+
+        pocet_row = {k: v for k, v in sorted(pocet_row.items(), key=lambda item: item[0])}
+
+        max_row = max(pocet_row)
+
+        table = [[[] for _ in range(len(perm_key))] for _ in range(max_row)]
+        columns = []
+        perm_copy = perm_key.copy()
+
+        for item in pocet_row.items():
+            value = item[1]
+            columns.append(text[:value])
+            del text[:value]
+
+        #prejdi perm_key, ziskavaj stlpce
+        for index in perm_key:
+            minimum = min(perm_key)
+            index_of_min = perm_key.index(minimum)
+            perm_key[index_of_min] = 9999
+            for index, letter_to_row in enumerate(columns[minimum]):
+                table[index][index_of_min] = letter_to_row
+
+        decoded_text = ""    
+        for row in table:
+            for letter in row:
+                if letter in self.alphabet:
+                    decoded_text += letter 
+
+        return decoded_text
+
+#neuplna tabulka
 cipher = Simple()
 text = 'kdyz se na svete objevi opravdovy genius poznate ho podle toho ze se proti nemu spiknou vsichni hlupaci'
 message = cipher.encode(text, 'neprecte toooo')
 print(message)
+text = cipher.decode(message, 'neprecte toooo')
+print(text)
